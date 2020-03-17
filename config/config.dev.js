8402c6a0753426c1e81597d1285916d800f8c020
zhanghaoxu <872297298@qq.com>
2020-03-17 23:33:39 +0800

    配置

--- config/config.dev.js
+++ config/config.dev.js
@@ -5,6 +5,19 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+// mysql setting
+const LOCAL_MYSQL_HOST = '207.246.127.110';
+const LOCAL_MYSQL_PORT = 3306;
+const LOCAL_MYSQL_USER = 'user';
+const LOCAL_MYSQL_PASSWORD = 'xuxuxu123';
+const LOCAL_MYSQL_DATABASE = 'moon';
+// redis setting
+// host: '10.177.0.81', // Redis host
+// password: 'ujbT3k3M4bW8LMTa',
+const LOCAL_REDIS_PORT = 6379;
+const LOCAL_REDIS_HOST = '207.246.127.110';
+const LOCAL_REDIS_PASSWORD = 'xuxuxu123';
+const LOCAL_REDIS_DB = 1;
 module.exports = appInfo => {
   /**
    * built-in config
