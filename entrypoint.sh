934559dc2886d766f7d887f7d7abcd95973c28df
zhanghaoxu <872297298@qq.com>
2020-03-18 15:15:26 +0800

    docker 脚本修改

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,9 +38,4 @@ case "$CMD" in
     exec npm start
     ;;
 
-   * )
-    # Run custom command. Thanks to this line we can still use
-    # "docker run our_image /bin/bash" and it will work
-    exec $CMD ${@:2}
-    ;;
 esac
