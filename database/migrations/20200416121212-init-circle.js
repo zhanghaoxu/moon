67d1bd537a78ced907eee6a76bb692b7627026d6
zhanghaoxu <872297298@qq.com>
2020-06-04 21:58:07 +0800

    ss

--- database/migrations/20200416121212-init-circle.js
+++ database/migrations/20200416121212-init-circle.js
@@ -1,23 +1,30 @@
 'use strict';
-
+const tableName = 'circle';
+const { name, info, options, createSuccessCallback } = require(`../tables/${tableName}`);
 module.exports = {
   up: (queryInterface, Sequelize) => {
+
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
+      Example:*/
+    return queryInterface.createTable(
+      name,
+      info,
+      options
+    ).then(() => {
+      createSuccessCallback(queryInterface);
+    });
 
-  down: (queryInterface, Sequelize) => {
+  },
+  down: queryInterface => {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
+      Example:*/
+    return queryInterface.dropTable(tableName);
+
+  },
 };
